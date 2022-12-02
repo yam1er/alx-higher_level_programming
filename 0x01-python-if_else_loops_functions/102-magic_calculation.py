9dcd4bce327718fe2409e19d08ec55514779f004
yam1er <mantynouyacoubou@gmail.com>
2022-12-02 04:23:02 -0800

    fix#1

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,6 +4,6 @@ def magic_calculatation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
-    if c > d:
+    if c > b:
         return (a + b)
     return (a*b - c)
