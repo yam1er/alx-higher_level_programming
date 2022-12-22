0f1bdbb19cbb2a32ed46e78180e5a9c39dba1c67
yam1er <mantynouyacoubou@gmail.com>
2022-12-21 16:47:47 -0800

    fix#1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+"""Define a class Square."""
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+class Square:
+    """Represent a square."""
+    pass
