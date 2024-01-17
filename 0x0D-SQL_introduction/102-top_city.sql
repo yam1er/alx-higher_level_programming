3ee201e45fb4e9241b52436913a28bb29543c4fb
yam1er <mantynouyacoubou@gmail.com>
2024-01-17 08:33:05 -0800

    fix

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Display top 3 of cities temperature during July an August
-SELECT `city` AVG(`value`) AS `avg_temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
