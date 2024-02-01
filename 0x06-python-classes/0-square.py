2ab037e94c72554f55bdbe821fcde35662641a9a
yam1er <mantynouyacoubou@gmail.com>
2024-01-31 17:40:53 -0800

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,5 @@
 """Define a class square."""
 
 class Square:
-    """square's class, no methode or another things"""
+    """square's class, no methode or another things empty class Square that defines a square."""
     pass
