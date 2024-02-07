eca90503df27e5da9f25ea84dec5252bc6b3ddb3
yam1er <mantynouyacoubou@gmail.com>
2024-02-07 05:47:36 -0800

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Define a classe square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a square instance
+        """Initialize a new square.
 
         Args:
-        size (int): The size of the square instance
-        position (int, int): Position of square instance
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square"""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -30,7 +30,7 @@ class Square:
 
     @property
     def position(self):
-        """Get/set the current position of the square"""
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -39,16 +39,15 @@ class Square:
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive",
-                            "integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """"Return the current area of the square"""
+        """Return the current area of the square."""
         return (self.__size ** 2)
 
     def my_print(self):
-        """Print the square with #"""
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
