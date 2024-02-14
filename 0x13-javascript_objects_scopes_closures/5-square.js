88788e74fbca1f6604949b9d4215f09340829bbb
yam1er <mantynouyacoubou@gmail.com>
2024-02-14 08:10:21 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,9 +3,4 @@ module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
-};#!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
 };
