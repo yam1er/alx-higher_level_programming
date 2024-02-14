177d93f84c7ad7ef266b12ce7cee44ab4350f020
yam1er <mantynouyacoubou@gmail.com>
2024-02-14 08:07:20 -0800

    Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+};#!/usr/bin/node
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+};ddssh 0737322d2bb0@0737322d2bb0.c796e532.alx-cod.online
